0f8a3f05453fc938373346788e5e75d2fdf06014
Will Slotkin <william.slotkin@gmail.com>
2016-05-10 15:07:02 +0200

    Revert "Reduce usage of avro reader to only evaluating persisted schema"
    This reverts commit 640ed746dd07c85f24a7576b56e5c8f8d9a320d0.

--- src/main/java/avrotools/FullAvroSerializationMain.java
+++ src/main/java/avrotools/FullAvroSerializationMain.java
@@ -1,10 +1,12 @@
 package avrotools;
 
+import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.dataformat.avro.AvroMapper;
 import com.fasterxml.jackson.dataformat.avro.AvroSchema;
 import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
 import org.apache.avro.reflect.ReflectData;
 import org.apache.avro.reflect.ReflectDatumReader;
@@ -12,7 +14,7 @@ import org.apache.avro.reflect.ReflectDatumWriter;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
+import java.nio.ByteBuffer;
 
 public class FullAvroSerializationMain {
 
@@ -41,14 +43,14 @@ public class FullAvroSerializationMain {
 
     private static void readAvroViaJackson() throws IOException {
         System.out.println("Deserializing avro via jackson");
-        File file = new File("users.avro");
+        DatumReader<ImmutableUser> userDatumReader = new ReflectDatumReader<>(ImmutableUser.class);
+        DataFileReader<ImmutableUser> dataFileReader = new DataFileReader<>(new File("users.avro"), userDatumReader);
 
-        DataFileReader<ImmutableUser> fileReader = new DataFileReader<>(file, new ReflectDatumReader<>(ImmutableUser.class));
-        AvroSchema persistedSchema = new AvroSchema(fileReader.getSchema());
-
-        Iterator<User> myIterator = new AvroMapper().readerFor(User.class).with(persistedSchema).readValues(file);
-        while (myIterator.hasNext()) {
-            User user = myIterator.next();
+        AvroSchema persistedSchema = new AvroSchema(dataFileReader.getSchema());
+        ObjectReader reader = new AvroMapper().readerFor(User.class).with(persistedSchema);
+        while (dataFileReader.hasNext()) {
+            ByteBuffer byteBuffer = dataFileReader.nextBlock();
+            Object user = reader.readValue(byteBuffer.array());
             System.out.println(user);
         }
         System.out.println("done\n");
